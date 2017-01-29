0e7cafed95a76b3fa5a6232a6325fb095a6950fb
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-29 11:11:44 -0700

    i=Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search-form/search-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html'
+})
+export class SearchFormComponent { }
